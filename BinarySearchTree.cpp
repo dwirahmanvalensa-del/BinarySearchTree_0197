bf0cc8f4e516fedb63c5c1ecd695f6f1dd1a0307
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-06-12 15:44:13 +0800

    membuat fungsi postorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -106,4 +106,22 @@ public:
         }
     }
 
-    
+    void postorder(Node *ptr)
+    {
+        // perfoms the postorder traveersal of the tree
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftChild);
+            postorder(ptr->rightChild);
+            cout << ptr->info << " ";
+        }
+    }
+
+};
+
